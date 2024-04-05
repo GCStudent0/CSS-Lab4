8bebaa11304471779ff8a9ee8d89afef814aa0c4
GCStudent0 <159974766+GCStudent0@users.noreply.github.com>
2024-04-05 12:03:09 -0400

    Add files via upload

---
+++ js/tables.js
@@ -0,0 +1,5 @@
+// STEP 11: Call to tablesorter script here
+const consoles = document.querySelector("table");
+console.log(consoles);
+let myTable = new Tablesort(consoles);
+// new Tablesort(document.getElementById("consoleData"));
